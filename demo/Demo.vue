0a3855babb5c50e390e73ff4a8c7f803d41f7c67
yooshikudozo <electrosheep910@gmail.com>
2020-09-21 03:01:37 +0900

    Create demo

--- demo/Demo.vue
+++ demo/Demo.vue
@@ -1,17 +1,39 @@
 <template>
   <div class="demo">
     <h1>Plugin Demo</h1>
-    <p>add: {{ a }} + {{ b }} = {{ $add(a, b) }}</p>
+
+    <section>
+      <header><h2>Pixel drawing</h2></header>
+      <div class="body">
+        <pixel-drawing />
+      </div>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import PixelDrawing from "./components/PixelDrawing.vue";
 
 export default Vue.extend({
   name: "demo",
-  data: () => ({ a: 1, b: 1 })
+
+  components: {
+    PixelDrawing
+  }
 });
 </script>
 
-<style></style>
+<style>
+.rainbow-pixel-canvas {
+  width: 400px;
+  height: 200px;
+  border: solid 2px gray;
+  box-sizing: border-box;
+  background: white;
+
+  user-select: none;
+  -ms-touch-action: none;
+  touch-action: none;
+}
+</style>
