c1b82afc814c9c0892d443922602e2a85a30d4c4
yoroshikudozo <electrosheep910@gmail.com>
2020-10-10 21:49:10 +0900

    Add tests

--- tests/unit/plugin.spec.ts
+++ tests/unit/plugin.spec.ts
@@ -1,17 +1,58 @@
-import { shallowMount, createLocalVue } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
-import plugin from "../../src/index";
-import { PluginAddFunction } from "../../types";
+import getInteractComponent from "@/interact";
+import { createLocalVue, mount } from "@vue/test-utils";
+import interact from "interactjs";
+import VueInteractjs from "../../src";
 
-describe("Plugin", () => {
-  it("should be 2", () => {
-    const msg = "new message";
+describe("Component", () => {
+  it("should can mount the interact with global component/options", () => {
     const localVue = createLocalVue();
-    localVue.use(plugin);
-    const wrapper = shallowMount(HelloWorld, {
-      localVue,
-      propsData: { msg }
+    localVue.use(VueInteractjs);
+
+    const interactComponent = getInteractComponent(interact);
+
+    const logs: string[] = [];
+    const initLog = "inited";
+
+    const wrapper = mount(interactComponent, {
+      propsData: {
+        draggable: true
+      },
+      listeners: {
+        ready() {
+          logs.push(initLog);
+        }
+      }
     });
-    expect(wrapper.vm.$add(1, 1)).toBe(2);
+
+    expect(logs[0]).toBe(initLog);
+
+    // @deprecated
+    // expect(wrapper.name()).toBe("Interact");
+    expect(wrapper.classes()).toContain("interact");
+    expect(wrapper.isVisible()).toBeTruthy();
+    expect(wrapper.find("div")).toBeTruthy();
+
+    const vm = wrapper.vm;
+
+    // @deprecated
+    // expect(vm.interactInstance).toBeInstanceOf(interact);
+    expect(Object.keys(wrapper.props())).toContain("draggable");
+    expect(typeof vm.draggable).toBe("boolean");
+    expect(wrapper.props().draggable).toBe(true);
+    expect(Object.keys(wrapper.props())).toContain("resizable");
+    expect(typeof vm.resizable).toBe("boolean");
+    expect(wrapper.props().resizable).toBe(false);
+    expect(Object.keys(wrapper.props())).toContain("droppable");
+    expect(typeof vm.droppable).toBe("boolean");
+    expect(wrapper.props().droppable).toBe(false);
+    expect(Object.keys(wrapper.props())).toContain("gesturable");
+    expect(typeof vm.gesturable).toBe("boolean");
+    expect(wrapper.props().gesturable).toBe(false);
+
+    expect(typeof vm.init).toBe("function");
+    expect(typeof vm.initDrag).toBe("function");
+    expect(typeof vm.initResize).toBe("function");
+    expect(typeof vm.initDrop).toBe("function");
+    expect(typeof vm.initGesture).toBe("function");
   });
 });
